f6e7cca5835c30bef797a9b12efeaab78967f050
Tri Pham <44308446+TriPham2110@users.noreply.github.com>
2020-10-14 18:14:22 -0500

    added requirements and minor updates (#3)

--- coms/Main.py
+++ coms/Main.py
@@ -4,19 +4,19 @@ import JsonHandler
 import time
 from Apps import WindowsVolumeMixerControl
 
+
 def main():
-    
-    print("Device Not Found")
-    port = None
     port = PortsHandler.findPort()
-    while(None == port):
+    while port is None:
+        print("Device Not Found")
         port = PortsHandler.findPort()
         time.sleep(10)
     print("Device has been found on port", port)
     s = SerialHandler.connectPort(port)
-    
+
     # The listen will infinite loop in the handler
     SerialHandler.listen(s)
-        
+
+
 if __name__ == '__main__':
     main()
