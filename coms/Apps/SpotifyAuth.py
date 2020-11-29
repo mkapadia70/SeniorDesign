2c18b6e5f854196016e9fa7d18ba02b9a811dad1
Max Goad <maxdgoad@gmail.com>
2020-11-28 23:11:14 -0800

    cleanup ignored files

--- coms/Apps/SpotifyAuth.py
+++
@@ -1,7 +0,0 @@
-from spotipy.oauth2 import SpotifyOAuth
-
-auth_manager = SpotifyOAuth(
-    scope=scope,
-    client_id='<youridhere>', 
-    client_secret='<yoursecrethere>', 
-    redirect_uri='<yourredirecturi>')
