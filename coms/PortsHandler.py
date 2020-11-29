1425a4331b43505baef294404f72a87248452fe9
Max Goad <maxdgoad@gmail.com>
2020-11-28 23:53:05 -0800

    made backend folder

--- coms/PortsHandler.py
+++ coms/PortsHandler.py
@@ -8,10 +8,3 @@ def checkConnected(port):
         if p.device == port:
             return True
     return False
-
-
-def findPort():
-    ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        if ("Prolific USB-to-Serial Comm Port" in p.description) or ("com0com" in p.description):
-            return p.device
